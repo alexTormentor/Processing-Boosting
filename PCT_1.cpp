710104edbb4401d8ba66614e16af15db3d0ac2aa
Alex <alexanderbor0134@mail.ru>
2025-02-17 14:33:40 +0300

    parallel things

---
+++ PCT_1.cpp
@@ -0,0 +1,216 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <math.h>
+
+// Определение типа для локальной матрицы
+typedef struct {
+    int rows;
+    int cols;
+    double* data;
+} LOCAL_MATRIX_TYPE;
+
+// Функция для выделения памяти под локальную матрицу
+LOCAL_MATRIX_TYPE* Local_matrix_allocate(int rows, int cols) {
+    LOCAL_MATRIX_TYPE* local_matrix = (LOCAL_MATRIX_TYPE*)malloc(sizeof(LOCAL_MATRIX_TYPE));
+    local_matrix->rows = rows;
+    local_matrix->cols = cols;
+    local_matrix->data = (double*)malloc(rows * cols * sizeof(double));
+    return local_matrix;
+}
+
+// Функция для инициализации локальной матрицы нулями
+void Set_to_zero(LOCAL_MATRIX_TYPE* local_matrix) {
+    int i, j;
+    for (i = 0; i < local_matrix->rows; i++) {
+        for (j = 0; j < local_matrix->cols; j++) {
+            local_matrix->data[i * local_matrix->cols + j] = 0.0;
+        }
+    }
+}
+
+// Функция для умножения двух локальных матриц
+void Local_matrix_multiply(LOCAL_MATRIX_TYPE* local_A, LOCAL_MATRIX_TYPE* local_B, LOCAL_MATRIX_TYPE* local_C) {
+    int i, j, k;
+    for (i = 0; i < local_A->rows; i++) {
+        for (j = 0; j < local_B->cols; j++) {
+            for (k = 0; k < local_A->cols; k++) {
+                local_C->data[i * local_C->cols + j] += local_A->data[i * local_A->cols + k] * local_B->data[k * local_B->cols + j];
+            }
+        }
+    }
+}
+
+// Определение структуры для информации о сетке
+typedef struct {
+    int p; // Общее число процессов
+    MPI_Comm comm; // Коммуникатор для сетки
+    MPI_Comm row_comm; // Коммуникатор строки
+    MPI_Comm col_comm; // Коммуникатор столбца
+    int q; // Порядок сетки
+    int my_row; // Номер строки
+    int my_col; // Номер столбца
+    int my_rank; // Ранг процесса в коммуникаторе сетки
+} GRID_INFO_TYPE;
+
+// Функция для настройки сетки
+void Setup_grid(GRID_INFO_TYPE* grid) {
+    int old_rank;
+    int dimensions[2];
+    int periods[2];
+    int coordinates[2];
+    int varying_coords[2];
+
+    // Настройка глобальной информации о сетке
+    MPI_Comm_size(MPI_COMM_WORLD, &(grid->p));
+    MPI_Comm_rank(MPI_COMM_WORLD, &old_rank);
+    grid->q = (int)sqrt((double)grid->p);
+    dimensions[0] = dimensions[1] = grid->q;
+    periods[0] = periods[1] = 1;
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dimensions, periods, 1, &(grid->comm));
+    MPI_Comm_rank(grid->comm, &(grid->my_rank));
+    MPI_Cart_coords(grid->comm, grid->my_rank, 2, coordinates);
+    grid->my_row = coordinates[0];
+    grid->my_col = coordinates[1];
+
+    // Настройка коммуникаторов для строк и столбцов
+    varying_coords[0] = 0;
+    varying_coords[1] = 1;
+    MPI_Cart_sub(grid->comm, varying_coords, &(grid->row_comm));
+    varying_coords[0] = 1;
+    varying_coords[1] = 0;
+    MPI_Cart_sub(grid->comm, varying_coords, &(grid->col_comm));
+}
+
+// Функция для умножения матриц с использованием алгоритма Фокса
+void Fox(int n, GRID_INFO_TYPE* grid, LOCAL_MATRIX_TYPE* local_A, LOCAL_MATRIX_TYPE* local_B, LOCAL_MATRIX_TYPE* local_C) {
+    LOCAL_MATRIX_TYPE* temp_A;
+    int step;
+    int bcast_root;
+    int n_bar; // Порядок подматрицы = n/q
+    int source;
+    int dest;
+    int tag = 43;
+    MPI_Status status;
+
+    n_bar = n / grid->q;
+    Set_to_zero(local_C);
+
+    // Вычисление адресов для циклического сдвига B
+    source = (grid->my_row + 1) % grid->q;
+    dest = (grid->my_row + grid->q - 1) % grid->q;
+
+    // Выделение памяти для рассылки блоков A
+    temp_A = Local_matrix_allocate(n_bar, n_bar);
+
+    for (step = 0; step < grid->q; step++) {
+        bcast_root = (grid->my_row + step) % grid->q;
+        if (bcast_root == grid->my_col) {
+            MPI_Bcast(local_A->data, local_A->rows * local_A->cols, MPI_DOUBLE, bcast_root, grid->row_comm);
+            Local_matrix_multiply(local_A, local_B, local_C);
+        }
+        else {
+            MPI_Bcast(temp_A->data, temp_A->rows * temp_A->cols, MPI_DOUBLE, bcast_root, grid->row_comm);
+            Local_matrix_multiply(temp_A, local_B, local_C);
+        }
+
+        MPI_Send(local_B->data, local_B->rows * local_B->cols, MPI_DOUBLE, dest, tag, grid->col_comm);
+        MPI_Recv(local_B->data, local_B->rows * local_B->cols, MPI_DOUBLE, source, tag, grid->col_comm, &status);
+    }
+
+    // Освобождение выделенной памяти
+    free(temp_A->data);
+    free(temp_A);
+}
+
+
+int main(int argc, char* argv[]) {
+    int n = 4; // Размер матрицы (пример: 4x4)
+    MPI_Init(&argc, &argv);
+
+    int p; // Общее число процессов
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+    if (p < 4) {
+        fprintf(stderr, "Ошибка: Необходимо как минимум 4 процесса\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    GRID_INFO_TYPE grid;
+    Setup_grid(&grid);
+
+    // Создание и инициализация локальных матриц
+    LOCAL_MATRIX_TYPE* local_A = Local_matrix_allocate(n / grid.q, n / grid.q);
+    LOCAL_MATRIX_TYPE* local_B = Local_matrix_allocate(n / grid.q, n / grid.q);
+    LOCAL_MATRIX_TYPE* local_C = Local_matrix_allocate(n / grid.q, n / grid.q);
+
+    // Заполнение local_A данными (пример)
+    for (int i = 0; i < local_A->rows; i++) {
+        for (int j = 0; j < local_A->cols; j++) {
+            local_A->data[i * local_A->cols + j] = 1.0; // Замените на свои данные
+        }
+    }
+
+    // Заполнение local_B данными (пример)
+    for (int i = 0; i < local_B->rows; i++) {
+        for (int j = 0; j < local_B->cols; j++) {
+            local_B->data[i * local_B->cols + j] = 1.0; // Замените на свои данные
+        }
+    }
+
+
+    Fox(n, &grid, local_A, local_B, local_C);
+
+
+    // Вычисление суммы на 0-м блоке строк
+    double local_sum = 0.0;
+    for (int i = 0; i < local_C->rows; i++) {
+        for (int j = 0; j < local_C->cols; j++) {
+            local_sum += local_C->data[i * local_C->cols + j];
+        }
+    }
+
+    double global_sum = 0.0;
+
+    // Используйте MPI_Reduce для суммирования на 0-м блоке строк
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, 0, grid.row_comm);
+
+    // global_sum теперь содержит сумму значений на 0-м блоке строк всех процессов
+
+    // Здесь вы можете вывести global_sum только на корневом процессе (процессе с рангом 0)
+    if (grid.my_rank == 0) {
+        printf("Sum on Process 0: %.2f\n", global_sum);
+    }
+
+
+    // Вывод содержимого local_C на каждом процессе после операции суммирования
+    for (int proc_row = 0; proc_row < grid.q; proc_row++) {
+        for (int proc_col = 0; proc_col < grid.q; proc_col++) {
+            if (grid.my_row == proc_row && grid.my_col == proc_col) {
+                // Текущий процесс, выводим содержимое local_C
+                printf("Local_C on Process [%d][%d]:\n", grid.my_row, grid.my_col);
+                for (int i = 0; i < local_C->rows; i++) {
+                    for (int j = 0; j < local_C->cols; j++) {
+                        printf("%.2f ", local_C->data[i * local_C->cols + j]);
+                    }
+                    printf("\n");
+                }
+            }
+
+            MPI_Barrier(grid.comm); // Синхронизация процессов
+        }
+    }
+
+
+
+    // Освобождение памяти
+    free(local_A->data);
+    free(local_A);
+    free(local_B->data);
+    free(local_B);
+    free(local_C->data);
+    free(local_C);
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ PCT_2.cpp
@@ -0,0 +1,214 @@
+﻿#include <stdio.h>
+#include <immintrin.h>
+#include <omp.h>
+
+#include <emmintrin.h>
+
+#define _FLOATING_POINT_
+#ifdef _FLOATING_POINT_
+//#define _DOUBLE_
+#define _FLOAT_
+#endif
+
+//#define _INTEGER_
+#ifdef _INTEGER_
+//#define _int8_
+//#define _int16_
+//#define _int32_
+#define _int64_
+#endif
+
+#define N 1000000 // 5000*5000
+
+void main(int argc, char** argv)
+{
+#ifdef _DOUBLE_
+	double* a = (double*)_aligned_malloc(N * N * sizeof(double), 16);
+	double* b = (double*)_aligned_malloc(N * N * sizeof(double), 16);
+	double* c = (double*)_aligned_malloc(N * N * sizeof(double), 16);
+#endif
+#ifdef _FLOAT_
+	float* a = (float*)_aligned_malloc(N * sizeof(float), 16);
+	float* b = (float*)_aligned_malloc(N * sizeof(float), 16);
+	float* c = (float*)_aligned_malloc(N * sizeof(float), 16);
+#endif
+#ifdef _int8_
+	unsigned char* a = (unsigned char*)_aligned_malloc(N * sizeof(unsigned char), 16);
+	unsigned char* b = (unsigned char*)_aligned_malloc(N * sizeof(unsigned char), 16);
+	unsigned char* c = (unsigned char*)_aligned_malloc(N * sizeof(unsigned char), 16);
+#endif
+#ifdef _int16_
+	short* a = (short*)_aligned_malloc(N * sizeof(short), 16);
+	short* b = (short*)_aligned_malloc(N * sizeof(short), 16);
+	short* c = (short*)_aligned_malloc(N * sizeof(short), 16);
+#endif
+#ifdef _int32_
+	int* a = (int*)_aligned_malloc(N * sizeof(int), 16);
+	int* b = (int*)_aligned_malloc(N * sizeof(int), 16);
+	int* c = (int*)_aligned_malloc(N * sizeof(int), 16);
+#endif
+#ifdef _int64_
+	__int64* a = (__int64*)_aligned_malloc(N * sizeof(__int64), 16);
+	__int64* b = (__int64*)_aligned_malloc(N * sizeof(__int64), 16);
+	__int64* c = (__int64*)_aligned_malloc(N * sizeof(__int64), 16);
+#endif
+
+	for (int i = 0; i < N; i++)
+	{
+		a[i] = 1;
+		b[i] = 2;
+		c[i] = 0;
+	}
+
+	double start = omp_get_wtime();
+	for (int i = 0; i < N; i++)
+		c[i] = a[i] * b[i] * a[i];
+	double end = omp_get_wtime();
+	printf("\n%f\n", end - start);
+
+	int step = 16;
+	start = omp_get_wtime();
+#ifdef _DOUBLE_
+	int num_iter = N / 2;
+#endif
+#ifdef _FLOAT_
+	int num_iter = N / 4;
+#endif
+#ifdef _int8_
+	int num_iter = N / 16;
+#endif
+#ifdef _int16_
+	int num_iter = N / 8;
+#endif
+#ifdef _int32_
+	int num_iter = N / 4;
+#endif
+#ifdef _int64_
+	int num_iter = N / 2;
+#endif
+	_asm
+	{
+		mov ESI, a // ТЕКУЩАЯ ПОЗИЦИЯ В а
+		mov EDI, b // ТЕКУЩАЯ ПОЗИЦИЯ В b
+		mov EDX, c // ТЕКУЩАЯ ПОЗИЦИЯ В c
+		mov ECX, num_iter
+		loop_vect :
+		movapd xmm0, xmmword ptr[ESI] // ТЕКУЩИЕ ЭЛЕМЕНТЫ a
+			movapd xmm1, xmmword ptr[EDI] // ТЕКУЩИЕ ЭЛЕМЕНТЫ b
+#ifdef _DOUBLE_
+			mulpd xmm1, xmm0 // умножаем
+			mulpd xmm1, xmm0 // умножаем
+#endif
+#ifdef _FLOAT_
+			mulps xmm1, xmm0
+			mulps xmm1, xmm0
+#endif
+#ifdef _int64_
+			paddq xmm1, xmm0
+			paddq xmm1, xmm0
+#endif 
+#ifdef _int32_
+			pmulld xmm1, xmm0
+			pmulld xmm1, xmm0
+#endif 
+#ifdef _int16_
+			pmullw xmm1, xmm0
+			pmullw xmm1, xmm0
+#endif 
+#ifdef _int8_
+			paddb xmm1, xmm0
+			paddb xmm1, xmm0
+#endif 
+			movapd xmmword ptr[EDX], xmm1 // СОХРАНЯЕМ в c
+			add ESI, step
+			add EDI, step
+			add EDX, step
+			loop loop_vect
+	}
+	end = omp_get_wtime();
+	printf("\nasm:\n");
+	/*for (int i = 0; i < N; i++)
+		printf("%d ", c[i]);*/
+	printf("\n%f\n", end - start);
+
+	start = omp_get_wtime();
+#ifdef _DOUBLE_
+	int step_i = 2;
+#endif
+#ifdef _FLOAT_
+	int step_i = 4;
+#endif
+#ifdef _int8_
+	int step_i = 16;
+#endif
+#ifdef _int16_
+	int step_i = 8;
+#endif
+#ifdef _int32_
+	int step_i = 4;
+#endif
+#ifdef _int64_
+	int step_i = 2;
+#endif
+#ifdef _DOUBLE_
+	__m128d Aij, Bij, Cij;
+	for (int i = 0; i < N; i += step_i)
+	{
+		Aij = _mm_load_pd(&a[i]);
+		Bij = _mm_load_pd(&b[i]);
+		Cij = _mm_mul_pd(Aij, Bij);
+		Cij = _mm_mul_pd(Cij, Aij);
+		_mm_store_pd(&c[i], Cij);
+	}
+#endif
+#ifdef _FLOAT_
+	__m128 Aij, Bij, Cij;
+	for (int i = 0; i < N; i += step_i)
+	{
+		Aij = _mm_load_ps(&a[i]);
+		Bij = _mm_load_ps(&b[i]);
+		Cij = _mm_mul_ps(Aij, Bij);
+		Cij = _mm_mul_ps(Cij, Aij);
+		_mm_store_ps(&c[i], Cij);
+	}
+#endif
+#ifdef _INTEGER_
+	__m128i Aij, Bij, Cij;
+	for (int i = 0; i < N; i += step_i)
+	{
+		Aij = _mm_load_si128((__m128i*) & a[i]);
+		Bij = _mm_load_si128((__m128i*) & b[i]);
+#ifdef _int8_
+		Cij = _mm_add_epi8(Aij, Bij);
+		Cij = _mm_add_epi8(Cij, Aij);
+#endif
+#ifdef _int16_
+		Cij = _mm_mullo_epi16(Aij, Bij);
+		Cij = _mm_mullo_epi16(Cij, Aij);
+#endif
+#ifdef _int32_
+		Cij = _mm_mullo_epi32(Aij, Bij);
+		Cij = _mm_mullo_epi32(Cij, Aij);
+#endif
+#ifdef _int64_
+		Cij = _mm_add_epi64(Aij, Bij);
+		Cij = _mm_add_epi64(Cij, Aij);
+#endif
+		_mm_store_si128((__m128i*) & c[i], Cij);
+	}
+#endif
+
+	end = omp_get_wtime();
+	printf("\nintr:\n");
+	/*for (int i = 0; i < N; i++)
+		printf("%d ", c[i]);*/
+	printf("\n%f\n", end - start);
+
+	start = omp_get_wtime();
+#pragma omp parallel for num_threads(4)
+	for (int i = 0; i < N; i++)
+		c[i] = a[i] * b[i] * a[i];
+	end = omp_get_wtime();
+	printf("\nomp:\n");
+	printf("\n%f\n", end - start);
+}
